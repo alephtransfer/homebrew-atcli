74e087a61f3e6cec2e9cc7f36f163d76547fd257
Younes HAFRI <younes.hafri@aleph-archives.com>
2021-05-15 21:26:06 +0200

    initial release

---
+++ formula/atcli.rb
@@ -0,0 +1,18 @@
+require "formula"
+
+class ATCLI < Formula
+  desc "Cross-Platform Secure High-Speed Managed File Transfer Service"
+  homepage "https://app.alephtransfer.com/"
+  url "https://github.com/spotim/fed-cli/releases/download/v1.0.3/atcli-darwin-amd64-1.0.3.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "4eeeb4d0dbe7a82f53a378b9ef9417c0ad2d12ab5db87135cb65ba04cc8bd3a9"
+  head "https://github.com/alephtransfer/homebrew-atcli.git"
+
+  def install
+    bin.install "atcli"
+  end
+
+  # Homebrew requires tests.
+  test do
+    assert_match "1.0.3", shell_output("#{bin}/atcli --version", 2)
+  end
+end
