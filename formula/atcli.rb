b6fabea5c9ee8650edacad8a54d32eaa4aef6fbc
alephtransfer <84237286+alephtransfer@users.noreply.github.com>
2021-05-16 23:11:37 +0200

    Update atcli.rb

--- formula/atcli.rb
+++ formula/atcli.rb
@@ -4,12 +4,16 @@ class Atcli < Formula
   version "1.0.3"
   desc "Cross-Platform Secure High-Speed Managed File Transfer Service"
   homepage "https://alephtransfer.com/"
+  
   license "proprietary"
   #license_link "https://alephtransfer.com/license/atcli/LICENSE.txt"
+  
   url "https://github.com/alephtransfer/homebrew-atcli/releases/download/v#{version}/atcli-darwin-amd64-#{version}.tar.gz"
   sha256 "7751ab7e3856b8601253a4575d254e066fb1e533b76995d3c45580170438d5a6"
   head "https://github.com/alephtransfer/homebrew-atcli.git"
-  depends_on macos: ">= :catalina"
+  
+  ## https://support.apple.com/en-us/HT201260
+  depends_on macos: ">= 10.14"
   
   livecheck do
     url :url
