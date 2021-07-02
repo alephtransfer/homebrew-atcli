bd1aa92628a689d5a2e7fedcbfcbf1a6832eb711
alephtransfer <84237286+alephtransfer@users.noreply.github.com>
2021-07-02 18:23:40 +0200

    Update atcli.rb
    Upgrade to 1.0.6
    
    - Fix download link prefix
    - Add URL Shortening feature

--- formula/atcli.rb
+++ formula/atcli.rb
@@ -10,7 +10,7 @@ class Atcli < Formula
   
   head "https://github.com/alephtransfer/homebrew-atcli.git"
   url "https://github.com/alephtransfer/homebrew-atcli/releases/download/v#{version}/atcli-darwin-amd64-#{version}.tar.gz"
-  sha256 "d84cfa602cf9800fd684db295b6b09cc9ba03fa6bbfb84e476ebd149a06cc7f3"
+  sha256 "553dfe707c896bf0216108241e9a17c1476d38a3d6028219157bdf7578fde255"
   
   ## https://support.apple.com/en-us/HT201260
   #depends_on macos: :big_sur
