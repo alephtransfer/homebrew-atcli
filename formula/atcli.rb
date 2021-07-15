6b3b206fb109b7a9d37eea3a77772647ec84c897
alephtransfer <84237286+alephtransfer@users.noreply.github.com>
2021-07-15 13:44:06 +0200

    Update atcli.rb

--- formula/atcli.rb
+++ formula/atcli.rb
@@ -10,7 +10,7 @@ class Atcli < Formula
   
   head "https://github.com/alephtransfer/homebrew-atcli.git"
   url "https://github.com/alephtransfer/homebrew-atcli/releases/download/v#{version}/atcli-darwin-amd64-#{version}.tar.gz"
-  sha256 "b7dd362d07cf656449671935c5c289bd7ee509314344c5b34cc88acc8ce0713f"
+  sha256 "0f5cd79d90af24dc49ee6212c35e61cff1dd181dfec1a899658ee29ca36acea4"
   
   ## https://support.apple.com/en-us/HT201260
   #depends_on macos: :big_sur
